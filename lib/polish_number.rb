33469772e53c7ab6df57f13fdc5770dfeecf9d17
Anna Kaminska <anna.kaminska@in4mates.com>
2017-10-05 16:54:26 +0200

    Ordinal numbers

--- lib/polish_number.rb
+++ lib/polish_number.rb
@@ -21,65 +21,79 @@ module PolishNumber
 
   MILLIONS = {:one => 'milion', :few => 'miliony', :many => 'milionów'}
 
+  CENTS = [:auto, :no, :words, :digits]
 
-  HUNDREDS_ORDINALS = ['', 'setny ', 'dwusetny ', 'trzysetny ', 'czterysetny ', 'pięćsetny ', 'sześćsetny ',
-                       'siedemsetny ', 'osiemsetny ', 'dziewięćsetny ']
+  CURRENCIES = {
+    :NO => {:one => '', :few => '', :many => '',
+            :one_100 => 'setna', :few_100 => 'setne', :many_100 => 'setnych', :gender_100 => :she},
+    :PLN => {:one => 'złoty', :few => 'złote', :many => 'złotych',
+            :one_100 => 'grosz', :few_100 => 'grosze', :many_100 => 'groszy'},
+    :USD => { :one => 'dolar', :few => 'dolary', :many => 'dolarów',
+            :one_100 => 'cent', :few_100 => 'centy', :many_100 => 'centów'},
+    :EUR => { :one => 'euro', :few => 'euro', :many => 'euro', :gender => :it,
+            :one_100 => 'cent', :few_100 => 'centy', :many_100 => 'centów'},
+    :GBP => { :one => 'funt', :few => 'funty', :many => 'funtów',
+            :one_100 => 'pens', :few_100 => 'pensy', :many_100 => 'pensów'},
+    :CHF => { :one => 'frank', :few => 'franki', :many => 'franków',
+            :one_100 => 'centym', :few_100 => 'centymy', :many_100 => 'centymów'},
+    :SEK => { :one => 'korona', :few => 'korony', :many => 'koron', :gender => :she,
+            :one_100 => 'öre', :few_100 => 'öre', :many_100 => 'öre', :gender_100 => :it}
+  }
 
-  TENS_ORDINALS = ['', 'dziesiąty ', 'dwudziesty ', 'trzydziesty ', 'czterdziesty ', 'pięćdziesiąty ',
-                   'sześćdziesiąty ', 'siedemdziesiąty ', 'osiemdziesiąty ', 'dziewięćdziesiąty ']
-
-  TEENS_ORDINALS = ['', 'jedenasty ', 'dwunasty ', 'trzynasty ', 'czternasty ', 'piętnasty ',
-                    'szesnasty ', 'siedemnasty ', 'osiemnasty ', 'dziewiętnasty ']
-
-  UNITIES_ORDINALS = ['', 'pierwszy ', 'drugi ', 'trzeci ', 'czwarty ', 'piąty ', 'szósty ', 'siódmy ', 'ósmy ',
-                      'dziewiąty ']
-
-  MASCULINE_PERSONAL_UNITIES = ['zerowi', 'pierwsi', 'drudzy', 'trzeci', 'czwarci']
-
-  ZERO_ORDINALS = 'zerowy'
-
-  TYPES = [:cardinal, :ordinal]
+  ORDINAL_MASCULINE_PERSONAL_EXCEPTION = ['zerowi', 'pierwsi', 'drudzy', 'trzeci', 'czwarci']
 
   GENDERS = [:masculine, :feminine, :neuter, :masculine_personal, :non_masculine]
 
   CASES = [:nominative, :genitive, :dative, :accusative, :instrumental, :locative, :vocative]
 
+  ORDINAL_TEENS_CORES = ["zerow",
+    "pierwsz",
+    "drugi",
+    "trzeci",
+    "czwart",
+    "piąt",
+    "szóst",
+    "siódm",
+    "ósm",
+    "dziewiąt",
+    "dziesiąt",
+    "jedenast",
+    "dwunast",
+    "trzynast",
+    "czternast",
+    "piętnast",
+    "szesnast",
+    "siedemnast",
+    "osiemnast",
+    "dziewiętnast"]
 
-  CENTS = [:auto, :no, :words, :digits]
+  ORDINAL_TENS_CORES = [
+    "zerow",
+    "dziesiąt", # wiem, wiem że to nie będzie używane, ale ładniej wygląda w tablicy :-)
+    "dwudziest",
+    "trzydziest",
+    "czterdziest",
+    "pięćdziesiąt",
+    "sześćdziesiąt",
+    "siedemdziesiąt",
+    "osiemdziesiąt",
+    "dziewięćdziesiąt"]
 
-  CURRENCIES = {
-      :NO => {:one => '', :few => '', :many => '',
-              :one_100 => 'setna', :few_100 => 'setne', :many_100 => 'setnych', :gender_100 => :she},
-      :PLN => {:one => 'złoty', :few => 'złote', :many => 'złotych',
-               :one_100 => 'grosz', :few_100 => 'grosze', :many_100 => 'groszy'},
-      :USD => { :one => 'dolar', :few => 'dolary', :many => 'dolarów',
-                :one_100 => 'cent', :few_100 => 'centy', :many_100 => 'centów'},
-      :EUR => { :one => 'euro', :few => 'euro', :many => 'euro', :gender => :it,
-                :one_100 => 'cent', :few_100 => 'centy', :many_100 => 'centów'},
-      :GBP => { :one => 'funt', :few => 'funty', :many => 'funtów',
-                :one_100 => 'pens', :few_100 => 'pensy', :many_100 => 'pensów'},
-      :CHF => { :one => 'frank', :few => 'franki', :many => 'franków',
-                :one_100 => 'centym', :few_100 => 'centymy', :many_100 => 'centymów'},
-      :SEK => { :one => 'korona', :few => 'korony', :many => 'koron', :gender => :she,
-                :one_100 => 'öre', :few_100 => 'öre', :many_100 => 'öre', :gender_100 => :it}
-  }
+  ORDINAL_FLEX = {
+    nominative: {masculine: 'y',feminine: 'a',neuter: 'e', non_masculine: 'e'},
+    genitive:  {masculine: 'ego',feminine: 'ej',neuter: 'ego', masculine_personal: 'ych', non_masculine: 'ych'},
+    dative: {masculine: 'emu',feminine: 'ej',neuter: 'emu', masculine_personal: 'ym', non_masculine: 'ym'},
+    accusative: {masculine: 'ego',feminine: 'ą',neuter: 'e', masculine_personal: 'ych', non_masculine: 'e'},
+    instrumental: {masculine: 'ym',feminine: 'ą',neuter: 'ym', masculine_personal: 'ymi', non_masculine: 'ymi'},
+    locative: {masculine: 'ym',feminine: 'ej',neuter: 'ym', masculine_personal: 'ych', non_masculine: 'ych'},
+    vocative: {masculine: 'y',feminine: 'a',neuter: 'e', non_masculine: 'e'}}
 
-  def self.validate(number, options)
-    if options[:currency] && !CURRENCIES.has_key?(options[:currency])
-      raise ArgumentError, "Unknown :currency option '#{options[:currency].inspect}'." +
-          " Choose one from: #{CURRENCIES.inspect}"
-    end
+  GRAMMAR_SUBSTITUTIONS = [{from: "iy", to: "i"},
+    {from: "gia", to: "ga"},
+    {from: "gią", to: "gą"}
+  ]
 
-    if options[:cents] && !CENTS.include?(options[:cents])
-      raise ArgumentError, "Unknown :cents option '#{options[:cents].inspect}'." +
-          " Choose one from: #{CENTS.inspect}"
-    end
-
-    if options[:type] && !TYPES.include?(options[:type])
-      raise ArgumentError, "Unknown :type option '#{options[:type].inspect}'." +
-          " Choose one from: #{TYPES.inspect}"
-    end
-
+  def self.validate_ordinal(number, options)
     if options[:gender] && !GENDERS.include?(options[:gender])
       raise ArgumentError, "Unknown :gender option '#{options[:gender].inspect}'." +
           " Choose one from: #{GENDERS.inspect}"
@@ -90,24 +104,53 @@ module PolishNumber
           " Choose one from: #{CASES.inspect}"
     end
 
-    unless options[:type] != :ordinal || (((0..999).include? number) && (number.is_a? Integer))
-      raise ArgumentError, 'for ordinal numbers, number should be integer and in 0..999 range'
+    if options[:gender] == :masculine_personal && (options[:grammatical_case] == :nominative || options[:grammatical_case] == :vocative) && ((0..4).include? number) == false
+      raise ArgumentError, 'for masculine personal gender and nominative/vocative case, number should be in 0..4 range'
     end
 
-    if options[:type] != :ordinal && options[:grammatical_case] == :instrumental
-      raise ArgumentError, 'this gem provides instrumental case only for ordinal numbers'
+    unless (0..99).include? number
+      raise ArgumentError, 'for ordinal type number should be in 0..999999999 range'
     end
 
-    if options[:grammatical_case] == :instrumental && options[:gender] != :masculine
-      raise ArgumentError, 'this gem provides only masculine gender for instrumental case'
+    options
+  end
+
+  def self.translate_ordinal(number, options={grammatical_case: :nominative, gender: :masculine})
+    options = validate_ordinal(number, options)
+
+    if options[:gender] == :masculine_personal && (options[:grammatical_case] == :nominative || options[:grammatical_case] == :vocative)
+      return ORDINAL_MASCULINE_PERSONAL_EXCEPTION[number]
     end
 
-    unless options[:grammatical_case] == :nominative || options[:grammatical_case] == :instrumental
-      raise ArgumentError, 'this gem provides only nominative and instrumental cases'
+    tens_digit = ((number.to_i / 10) % 10)
+    unit_digit = (number.to_i % 10)
+
+    if number < 20
+      final_ordinal = ORDINAL_TEENS_CORES[number] + ORDINAL_FLEX[options[:grammatical_case]][options[:gender]]
+    else
+      begining_of_final_ordinal = ORDINAL_TENS_CORES[tens_digit] + ORDINAL_FLEX[options[:grammatical_case]][options[:gender]]
+      if unit_digit == 0
+        final_ordinal = begining_of_final_ordinal
+      else
+        final_ordinal = begining_of_final_ordinal + " " +
+            ORDINAL_TEENS_CORES[unit_digit] + ORDINAL_FLEX[options[:grammatical_case]][options[:gender]]
+      end
+    end
+    GRAMMAR_SUBSTITUTIONS.each { |subst|
+      final_ordinal.sub!(subst[:from], subst[:to]);
+    }
+    return final_ordinal
+  end
+
+  def self.validate(number, options)
+    if options[:currency] && !CURRENCIES.has_key?(options[:currency])
+      raise ArgumentError, "Unknown :currency option '#{options[:currency].inspect}'." +
+                  " Choose one from: #{CURRENCIES.inspect}"
     end
 
-    if options[:type] == :ordinal && options[:gender] == :masculine_personal && ((0..4).include? number) == false
-      raise ArgumentError, 'for ordinal numbers and masculine personal gender, number should be in 0..4 range'
+    if options[:cents] && !CENTS.include?(options[:cents])
+      raise ArgumentError, "Unknown :cents option '#{options[:cents].inspect}'." +
+                  " Choose one from: #{CENTS.inspect}"
     end
 
     unless (0..999999999).include? number
@@ -118,13 +161,9 @@ module PolishNumber
 
   def self.translate(number, options={})
 
-    options[:cents] ||= :auto
-    options[:type] ||= :cardinal
-    options[:gender] ||= :masculine
-    options[:grammatical_case] ||= :nominative
-
     options = validate(number, options)
 
+    options[:cents] ||= :auto
     number = number.to_i if options[:cents]==:no
     formatted_number = sprintf('%012.2f', number)
     currency = CURRENCIES[options[:currency] || :NO]
@@ -154,7 +193,7 @@ module PolishNumber
           result << ' i '
         end
       end
-      result << process_0_999(digits_cents, number_cents, currency[:gender_100] || :hi, options, false) if digits
+      result << process_0_999(digits_cents, number_cents, currency[:gender_100] || :hi) if digits
       result << ZERO.dup if formatted_sub_number == '00'
       result.strip!
       result << ' '
@@ -170,43 +209,18 @@ module PolishNumber
 
   def self.process_1_999999999(digits, options, number, currency)
     if number == 0 || (number.to_i == 0 && [:words, :digits].include?(options[:cents]))
-      if options[:type] == :ordinal
-        result = ZERO_ORDINALS.dup
-        if result != ''
-          if options[:grammatical_case] == :nominative
-            if options[:gender] == :masculine
-              result
-            elsif options[:gender] == :feminine
-              result = result.reverse.sub(result[-1], 'a').reverse
-            elsif options[:gender] == :neuter || options[:gender] == :non_masculine
-              result = result.reverse.sub(result[-1], 'e').reverse
-            elsif options[:gender] == :masculine_personal
-              result = MASCULINE_PERSONAL_UNITIES[0]
-            end
-          elsif options[:grammatical_case] == :instrumental
-            if options[:gender] == :masculine
-              result = result + 'm'
-            else
-              ''
-            end
-          end
-        else
-          result
-        end
-      else
-        result = ZERO.dup
-      end
+      result = ZERO.dup
     else
       result = ''
-      result << process_0_999(digits[0..2], number, :number, options, false)
+      result << process_0_999(digits[0..2], number, :number)
       result << millions(number.to_i/1000000, digits[0..2])
       result.strip!
       result << ' '
-      result << process_0_999(digits[3..5], number, :number, options, false)
+      result << process_0_999(digits[3..5], number, :number)
       result << thousands(number.to_i/1000, digits[3..5])
       result.strip!
       result << ' '
-      result << process_0_999(digits[6..8], number, currency[:gender] || options[:gender] || :hi, options, true)
+      result << process_0_999(digits[6..8], number, currency[:gender] || :hi)
       result.strip!
     end
 
@@ -216,137 +230,31 @@ module PolishNumber
     result
   end
 
-  def self.process_0_999(digits, number, object, options, is_last)
+  def self.process_0_999(digits, number, object)
     result = ''
-    if options[:type] == :ordinal
-      if digits[1] == 0 && digits[2] == 0
-        hundred = HUNDREDS_ORDINALS[digits[0]]
-        if hundred != ''
-          if options[:grammatical_case] == :nominative
-            if options[:gender] == :masculine
-              result << hundred
-            elsif options[:gender] == :feminine
-              result << hundred.reverse.sub(hundred[-2], 'a').reverse
-            elsif options[:gender] == :neuter || options[:gender] == :non_masculine
-              result << hundred.reverse.sub(hundred[-2], 'e').reverse
-            else
-              result << hundred
-            end
-          elsif options[:grammatical_case] == :instrumental
-            if options[:gender] == :masculine
-              result << hundred.sub(' ', 'm ')
-            else
-              ''
-            end
-          end
-        end
-      else
-        result << HUNDREDS[digits[0]]
-      end
-      if digits[1] == 1 && digits[2] != 0
-        teen = TEENS_ORDINALS[digits[2]]
-        if teen != ''
-          if options[:grammatical_case] == :nominative
-            if options[:gender] == :masculine
-              result << teen
-            elsif options[:gender] == :feminine
-              result << teen.reverse.sub(teen[-2], 'a').reverse
-            elsif options[:gender] == :neuter || options[:gender] == :non_masculine
-              result << teen.reverse.sub(teen[-2], 'e').reverse
-            else
-              result << teen
-            end
-          elsif options[:grammatical_case] == :instrumental
-            if options[:gender] == :masculine
-              result << teen.sub(' ', 'm ')
-            else
-              ''
-            end
-          end
-        end
-      else
-        ten = TENS_ORDINALS[digits[1]]
-        if ten != ''
-          if options[:grammatical_case] == :nominative
-            if options[:gender] == :masculine
-              result << ten
-            elsif options[:gender] == :feminine
-              result << ten.reverse.sub(ten[-2], 'a').reverse
-            elsif options[:gender] == :neuter || options[:gender] == :non_masculine
-              result << ten.reverse.sub(ten[-2], 'e').reverse
-            else
-              result << ten
-            end
-          elsif options[:grammatical_case] == :instrumental
-            if options[:gender] == :masculine
-              result << ten.sub(' ', 'm ')
-            else
-              ''
-            end
-          end
-        end
-        result << process_0_9(digits, number, object, options, is_last)
-      end
-    else
-      result << HUNDREDS[digits[0]]
+    result << HUNDREDS[digits[0]]
 
-      if digits[1] == 1 && digits[2] != 0
-        result << TEENS[digits[2]]
-      else
-        result << TENS[digits[1]]
-        result << process_0_9(digits, number, object, options, is_last)
-      end
+    if digits[1] == 1 && digits[2] != 0
+      result << TEENS[digits[2]]
+    else
+      result << TENS[digits[1]]
+      result << process_0_9(digits, number, object)
     end
 
     result
   end
 
-  def self.process_0_9(digits, number, object, options, is_last)
-    if options[:type] == :ordinal
-      unity = UNITIES_ORDINALS[digits[2]]
-      if unity != ''
-        if options[:grammatical_case] == :nominative
-          if options[:gender] == :masculine
-            unity
-          elsif options[:gender] == :feminine
-            if unity == 'trzeci '
-              'trzecia '
-            else
-              unity.reverse.sub(unity[-2], 'a').reverse
-            end
-          elsif options[:gender] == :neuter || options[:gender] == :non_masculine
-            if unity[-2] == 'i'
-              unity.reverse.sub(unity[-1], ' e').reverse
-            else
-              unity.reverse.sub(unity[-2], 'e').reverse
-            end
-          elsif options[:gender] == :masculine_personal
-            MASCULINE_PERSONAL_UNITIES[digits[2]]
-          else
-            unity
-          end
-        elsif options[:grammatical_case] == :instrumental
-          if options[:gender] == :masculine
-            unity.sub(' ', 'm ')
-          else
-            ''
-          end
-        end
-      else
-        unity
-      end
+  def self.process_0_9(digits, number, object)
+    if digits[2] == 2 && object == :she
+      'dwie '
+    elsif number == 1 && object == :she
+      'jedna '
+    elsif number == 1 && object == :it
+      'jedno '
+    elsif digits == [0,0,1] && object == :number
+      ''
     else
-      if digits[2] == 2 && is_last && (object == :she || options[:gender] == :feminine)
-        'dwie '
-      elsif digits[2] != 0 && number == 1 && (object == :she || options[:gender] == :feminine)
-        'jedna '
-      elsif digits[2] != 0 && number == 1 && (object == :it || options[:gender] == :neuter)
-        'jedno '
-      elsif digits == [0,0,1] && object == :number
-        ''
-      else
-        UNITIES[digits[2]]
-      end
+      UNITIES[digits[2]]
     end
   end
 
@@ -370,7 +278,7 @@ module PolishNumber
     if number == 1
       return :one_100 if cents
       :one
-      # all numbers with 2, 3 or 4 at the end, but not teens
+    # all numbers with 2, 3 or 4 at the end, but not teens
     elsif digits && (2..4).include?(digits[-1]) && digits[-2] != 1
       return :few_100 if cents
       :few
